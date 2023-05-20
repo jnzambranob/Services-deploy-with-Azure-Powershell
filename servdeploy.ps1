20472d32eb526b1a5f83e9529d49b78abc68813c
Nicolas Zambrano <josexp75@gmail.com>
2023-05-20 05:14:28 -0500

    Update servdeploy.ps1

--- servdeploy.ps1
+++ servdeploy.ps1
@@ -1,74 +1,99 @@
 param ($ambient)
-function Set-UniqueName{
-    
+#FUNCTIONS DECLARATIONS
+function Find-IfResourceExist{    
     param (
-        [Parameter(Mandatory)]
-        [string]$UniqueName
+        [Parameter(Mandatory=$true)]$rgname,
+        [Parameter(Mandatory=$false)]$resname
     )
-
-    $date = Get-date; #date for unique name generation;
-    $uidate = "{0}{1}{2}-{3}{4}{5}" -f $date.Year,$date.Month,$date.Day,$date.hour,$date.Minute,$date.Second #Unique namegen
-    $UniqueNameOut = $UniqueName + "$uidate"
-    Write-Output $UniqueNameOut
+    if ( $null -eq $resname ){
+        $resourcecheck = Get-AzResourceGroup -ResourceGroupName $rgname
+        if ( $null -eq $resourcecheck ){
+            Write-Output $false
+        }
+        else {
+            Write-Output $true
+             }
+    }
+    else{
+        $resourcecheck = Get-AzResource -ResourceGroupName $rgname -Name $resname
+        if ( $null -eq $resourcecheck ){
+            Write-Output $false
+        }
+        else {
+            Write-Output $true
+             }
+    } 
 }
-
-if ($ambient -eq "dev"){ #setting names for DEVELOPMENT enviroment
+#-----------------------
+#CONNECTION CONFIRMATION
+Connect-AzAccount | Out-Null;
+#-----------------------
+#ENVIROMENT SETTINGS
+#----------------------DEVELOPMENT-----------------------
+if ($ambient -eq "dev"){ #setting names for development enviroment
     write-host "Enviroment: Development"
     $rgname = "RGBootcamp-Dev" #Resource Group name
-    $spname = "SPBootcamp-Dev" #Service Plan name
-    $appname = Set-UniqueName ASBootcamp-Dev #Appservice Plan name
-    $fname =  Set-UniqueName FuncBootcamp-Dev #Function Service name
+    $spname = "SPBootcamp-Dev" #App Service Plan name
+    $appname = "ASBootcamp-Dev" #Appservice name (unique name required)
+    $fname =  "FuncBootcamp-Dev" #Function Service name (unique name required)
     $location = "East US" #Location of the resources
+    $stoname = "stobootcampdev" #Storage account name (Mandatory for correct configuration of function app)
 }
-elseif ($ambient -eq "prod") { #setting names for PRODUCTION enviroment
+#----------------------PRODUCTION-----------------------
+elseif ($ambient -eq "prod") { #setting names for production enviroment
     write-host "Enviroment: Production"
     $rgname = "RGBootcamp-Prod" #Resource Group name
-    $spname = "SPBootcamp-Prod" #Service Plan name
-    $appname = Set-UniqueName ASBootcamp-Prod #Appservice Plan name
-    $fname =  Set-UniqueName FuncBootcamp-Prod #Function Service name
-    $location = "East US" #Location of the resources    
+    $spname = "SPBootcamp-Prod" #App Service Plan name
+    $appname = "ASBootcamp-Prod" #Appservice name (unique name required)
+    $fname =  "FuncBootcamp-Prod" #Function Service name (unique name required)
+    $location = "East US" #Location of the resources
+    $stoname = "stobootcampprod" #(Mandatory for correct configuration of function app)
 }
 else{
     write-host "The parameter for execution is not recognized or missing, check and try again"
     exit
 }
-
-Connect-AzAccount | Out-Null;
-
-$Appserplanprop = @{ #Properties of the App service plan /serverfarms
-    Location          = $location
-    Properties        = @{Createdby="AZPowershell"}
-    Sku               = @{name= "F1"
-                          tier = "Free"
-                          size = "F1"
-                          family = "F"
-                          capacity = 1
-                         }
-    ResourceName      = $spname
-    ResourceType      = "Microsoft.Web/serverfarms"
-    ResourceGroupName = $rgname
-    Force             = $true
-}
+#-----------------------
+#RESOURCES DECLARATION AND DEPLOY
+#-----------------------RESOURCE GROUP------------------------
 New-AzResourceGroup -Name $rgname -Location $location -Force #Resource Group deployment
-New-AzResource @Appserplanprop #App service plan deployment
-$sf = Get-AzResource -Name $spname;
+#-----------------------RESOURCE: APP SERVICE PLAN------------------------
+$Appserplanprop = @{ #Properties of the App service plan /serverfarms
+    ResourceGroupName = $rgname
+    Name              = $spname
+    Location          = $location
+    Tier              = "Free"
+}
+New-AzAppServicePlan @Appserplanprop #App service plan deployment
+#-----------------------RESOURCE: APP SERVICE------------------------
 $Appserprop = @{ #Properties of the App Service /sites
-    Location          = $location
-    Properties        = @{Createdby="AZPowershell"
-                          serverFarmId=$sf.Id}
-    ResourceName      = $appname
-    ResourceType      = "microsoft.Web/sites"
     ResourceGroupName = $rgname
-    Force             = $true
+    Name              = $appname
+    Location          = $location
+    AppServicePlan    = $spname
 }
-New-AzResource @Appserprop #App service deployment
+if((Find-IfResourceExist $rgname $appname) -eq $false){
+    Write-Output "Creating resource"
+    New-AzWebApp @Appserprop #App service deployment
+}
+else {
+    Write-Output "The resource exist, skipping deployment"
+}
+#-----------------------RESOURCE: FUNCTION APP------------------------
 $Funcprop = @{ #Properties of the App Service /sites
-    Location          = $location
-    Kind              = "functionapp"
-    Properties        = @{Createdby="AZPowershell"}
-    ResourceName      = $fname
-    ResourceType      = "microsoft.Web/sites"
-    ResourceGroupName = $rgname
-    Force             = $true
+    Location           = $location  #For NewAzFunctionApp
+    Name               = $fname     #For NewAzFunctionApp
+    ResourceGroupName  = $rgname    #For NewAzFunctionApp
+    Runtime            = "powershell"
+    StorageAccountName = $stoname
 }
-New-AzResource @Funcprop #Function deployment
+if((Find-IfResourceExist $rgname $fname) -eq $false){
+    Write-Output "Creating storage account"
+    New-AzStorageAccount -ResourceGroupName $rgname -Name $stoname -Location $location -SkuName Standard_LRS
+    Write-Output "Creating resource"
+    New-AzFunctionApp @Funcprop #General Function deployment (avoid the manual deploy of storage account)
+}
+else {
+    Write-Output "The resource exist, skipping deployment"
+}
+#------------------------END OF THE SCRIPT----------------------------
